3f625e844ebce5b4442e9203ed2673dfb0a60751
Max Kiselyov <maxkiselyoff@gmail.com>
2021-03-27 00:51:00 +0500

    added CardView struct

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -12,11 +12,7 @@ struct ContentView: View {
         HStack {
             
             ForEach (0..<4) { index in
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                    Text("😇")
-                }
+                CardView()
             }
             
         }
@@ -26,8 +22,15 @@ struct ContentView: View {
     }
 }
 
-
-
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text("😇")
+        }
+    }
+}
 
 
 
