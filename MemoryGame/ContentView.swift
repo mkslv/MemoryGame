8895e3d5e16985f065a2b37c17526d418bcd4749
Max Kiselyov <maxkiselyoff@gmail.com>
2022-05-29 23:02:36 +0500

    Initial Commit

---
+++ MemoryGame/ContentView.swift
@@ -0,0 +1,105 @@
+//
+//  ContentView.swift
+//  MemoryGame
+//
+//  Created by Max Kiselyov on 5/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var emojis = ["🐲", "👹", "👾", "🤖", "👻", "👺", "🎃", "🦀"]
+    @State var emojiCount = 6
+    
+    var body: some View {
+        VStack{
+            HStack{
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(cardContent: emoji)
+                }
+            }
+            Spacer()
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            .padding(.horizontal)
+            .font(.largeTitle)
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 2{
+                emojiCount -= 2
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    var add: some View{
+        Button {
+            if emojiCount < emojis.count{
+                emojiCount += 2
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp: Bool = false
+    var cardContent: String
+    let shape = RoundedRectangle(cornerRadius: 20)
+    
+    var body: some View {
+        ZStack {
+                
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(cardContent).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+            
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Max Kiselyov on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
